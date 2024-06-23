a15d0ddd28d90fbe283ae546d0f3396c2db25fca
webgori <webgori@gmail.com>
2024-06-23 17:55:10 +0900

    숙제

---
+++ file6.py
@@ -0,0 +1,26 @@
+for n in range(5, 0, -1):
+  print(n)
+  
+number = int(input('임의의 양수를 입력하세요 >>>'))
+sum = 0
+
+for n in range(1, number + 1):
+  sum += n
+
+print(f'1부터 {number}사이 모든 정수의 합계는 {sum}입니다.')
+
+list = []
+number = int(input('몇 개의 과일을 보관할까요? >>>'))
+for n in range(1, 5 + 1):
+  list.append(input(f'{n}번째 과일을 입력하세요 >>>'))
+
+print(f'입력받은 과일들은 {list} 입니다.')
+
+
+exam = [99, 78, 100, 91, 81, 85, 54, 100, 71, 50]
+score = []
+
+for ex in exam:
+  score.append(min(100, ex + 5))
+  
+print(score)
